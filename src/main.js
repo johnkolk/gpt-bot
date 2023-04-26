16f4b355a7b09aa3dc6ece78e8ca68bfe00f1922
ivankolkovskiy <ivan.kolkovskii@gmail.com>
2023-04-26 22:44:42 +0300

    OpenAI class

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import config from "config";
 import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+import { code } from "telegraf/format";
 
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
@@ -11,13 +13,18 @@ const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
 bot.on(message("voice"), async (ctx) => {
   try {
+    await ctx.reply(code("Waiting response..."));
+
     const link = await ctx.telegram.getFileLink(ctx.message.voice);
     const userId = String(ctx.message.from.id);
 
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.toMp3(oggPath, userId);
 
-    await ctx.reply(mp3Path);
+    const text = await openai.voiceToText(mp3Path);
+    // const response = await openai.chat(text);
+
+    await ctx.reply(text);
   } catch (e) {
     console.log("Error while voice message ", e.message);
   }
--- src/openai.js
+++ src/openai.js
@@ -1,3 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+import config from "config";
+import { createReadStream } from "fs";
+
 class OpenAI {
-  constructor() {}
+  constructor(apiKey) {
+    const configuration = new Configuration({
+      apiKey,
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
+
+  chat() {}
+  async voiceToText(filePath) {
+    try {
+      const response = await this.openai.createTranscription(
+        createReadStream(filePath),
+        "whisper-1"
+      );
+      return response.data.text;
+    } catch (e) {
+      console.log("OpenAI voiceToText error ", e.message);
+    }
+  }
 }
+
+export const openai = new OpenAI(config.get("OPENAI_API_KEY"));
